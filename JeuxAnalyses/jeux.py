9815039fe65e189a9c55497722c912393da05b6b
FlorencePicard <florence.dellaniello-picard@polymtl.ca>
2023-08-02 13:46:15 -0400

    Ajout des calculs des autres jeux modifiés

--- JeuxAnalyses/jeux.py
+++ JeuxAnalyses/jeux.py
@@ -6,12 +6,9 @@ Created on Tue Aug  1 14:26:25 2023
 """
 
 class AWalkAcrossTheBoard():
-    def init(self):
-        nom_txt = '*WalkAcrossTheBoard*'  # NOM DANS FICHIER .TXT
+    def __init__(self):
+        self.nom_txt = '*WalkAcrossTheBoard*'  # NOM DANS FICHIER .TXT
         self.nom = "A Walk Across The Board"  # NOM DANS FICHIER .XSLX
-                    
-        return nom_txt, self.nom
-    
     
     def calculs(self, donnees, AAAA, MM, DD):
         duration = donnees[-1, 0] * 60
@@ -31,98 +28,105 @@ class AWalkAcrossTheBoard():
         return resultats
 
 
-# class ItalianAlps():
-#     def init(self):
-#         nom_txt = '*ItalianAlps*'  # NOM DANS FICHIER .TXT
-#         self.nom = "Italian alps"  # NOM DANS FICHIER .XSLX
-#         variables = ["Jeu",
-#                       "Date_AAAA_MM_DD",
-#                       "Duree_s",
-#                       "Distance_m",
-#                       "VitesseMax_m/s",
-#                       "VitesseMoy_m/s",
-#                       "InclinaisonMax_deg",
-#                       "InclinaisonMin_deg",
-#                       "InclinaisonMoy_deg",
-#                       ]  # NOM VARIABLES
+class ItalianAlps():
+    def __init__(self):
+        self.nom_txt = '*ItalianAlps*'  # NOM DANS FICHIER .TXT
+        self.nom = "Italian alps"  # NOM DANS FICHIER .XSLX
     
-#         return nom_txt, self.nom, variables
+    def calculs(self, donnees, AAAA, MM, DD):
+        duration = donnees[-1, 0] * 60
+        distance = donnees[-1, 2]
 
-#     def calculs(self, donnees, AAAA, MM, DD):
-#         duration = donnees[-1, 0] * 60
-#         distance = donnees[-1, 2]
+        maxSpeed = max(donnees[:, 1])
+        meanSpeed = (donnees[:, 1]).mean()
+        maxInclination = max(donnees[:, 5])
+        minInclination = min(donnees[:, 5])
+        meanInclination = (donnees[:, 5]).mean()
 
-#         maxSpeed = max(donnees[:, 1])
-#         meanSpeed = (donnees[:, 1]).mean()
-#         maxInclination = max(donnees[:, 5])
-#         minInclination = min(donnees[:, 5])
-#         meanInclination = (donnees[:, 5]).mean()
+        resultats = (("Jeu", self.nom), 
+                     ("Date_AAAA_MM_DD", f"{AAAA}_{MM}_{DD}"), 
+                     ("Duree_s", duration), 
+                     ("Distance_m", distance),
+                     ("VitesseMax_m/s", maxSpeed),
+                     ("VitesseMoy_m/s", meanSpeed),
+                     ("InclinaisonMax_deg", maxInclination),
+                     ("InclinaisonMin_deg", minInclination),
+                     ("InclinaisonMoy_deg", meanInclination)
+                    )
 
-#         resultats = [self.nom,
-#                      f"{AAAA}_{MM}_{DD}",
-#                      duration,
-#                      distance,
-#                      maxSpeed,
-#                      meanSpeed,
-#                      maxInclination,
-#                      minInclination,
-#                      meanInclination
-#                      ]  # Resume des variables
-
-#         return resultats
-
-# # def KiteFlyer():
-# #     return nom_txt, nom, variables
+        return resultats
 
 
-# def Microbes():
-#     nom_txt = '*Microbes*'  # NOM DANS FICHIER .TXT
-#     nom = "Microbes"  # NOM DANS FICHIER .XSLX
-#     variables = ["Jeu",
-#                  "Date_AAAA_MM_DD",
-#                  "Duree_s",
-#                  "Distance_m",
-#                  "VitesseMax_m/s",
-#                  "VitesseMoy_m/s",
-#                  ]  # NOM VARIABLES
+class Microbes():
+    def __init__(self):
+        self.nom_txt = '*Microbes*'  # NOM DANS FICHIER .TXT
+        self.nom = "Microbes"  # NOM DANS FICHIER .XSLX
+    
+    def calculs(self, donnees, AAAA, MM, DD):
+        duration = donnees[-1, 0] * 60
+        distance = donnees[-1, 2]
+    
+        maxSpeed = max(donnees[:, 1])
+        meanSpeed = (donnees[:, 1]).mean()
+    
+        resultats = (("Jeu", self.nom), 
+                     ("Date_AAAA_MM_DD", f"{AAAA}_{MM}_{DD}"), 
+                     ("Duree_s", duration), 
+                     ("Distance_m", distance),
+                     ("VitesseMax_m/s", maxSpeed),
+                     ("VitesseMoy_m/s", meanSpeed),
+                     )
 
-#     return nom_txt, nom, variables
+        return resultats
+    
 
+class PerturbationTrainer():
+    def __init__(self):
+        self.nom_txt = '*PerturbationTrainer*'  # NOM DANS FICHIER .TXT
+        self.nom = "Perturbation trainer"  # NOM DANS FICHIER .XSLX
+    
+    def calculs(self, donnees, AAAA, MM, DD):
+        duration = donnees[-1, 0] * 60
+        distance = donnees[-1, 2]
+    
+        maxSpeed = max(donnees[:, 6])
+        meanSpeed = (donnees[:, 6]).mean()
+    
+        resultats = (("Jeu", self.nom), 
+                     ("Date_AAAA_MM_DD", f"{AAAA}_{MM}_{DD}"), 
+                     ("Duree_s", duration), 
+                     ("Distance_m", distance),
+                     ("VitesseMax_m/s", maxSpeed),
+                     ("VitesseMoy_m/s", meanSpeed),
+                     )
 
-# def PerturbationTrainer():
-#     nom_txt = '*PerturbationTrainer*'  # NOM DANS FICHIER .TXT
-#     nom = "Perturbation Trainer"  # NOM DANS FICHIER .XSLX
-#     variables = ["Jeu",
-#                  "Date_AAAA_MM_DD",
-#                  "Duree_s",
-#                  "Distance_m",
-#                  "VitesseMax_m/s",
-#                  "VitesseMoy_m/s",
-#                  ]  # NOM VARIABLES
-
-#     return nom_txt, nom, variables
-
-
-# def RopeBridge():
-#     nom_txt = '*RopeBridge*'  # NOM DANS FICHIER .TXT
-#     nom = "Rope bridge"  # NOM DANS FICHIER .XSLX
-#     variables = ["Jeu",
-#                  "Date_AAAA_MM_DD",
-#                  "Duree_s",
-#                  "Distance_m",
-#                  "VitesseMax_m/s",
-#                  "VitesseMoy_m/s",
-#                  "InclinaisonMax_deg",
-#                  "InclinaisonMin_deg",
-#                  "InclinaisonMoy_deg",
-#                  ]  # NOM VARIABLES
-
-#     return nom_txt, nom, variables
-
-
-# # def TrafficJam():
-# #     return nom_txt, nom, variables
+        return resultats
 
 
-# # def TrafficJamLR():
-# #     return nom_txt, nom, variables
+class RopeBridge():
+    def __init__(self):
+        self.nom_txt = '*RopeBridge*'  # NOM DANS FICHIER .TXT
+        self.nom = "Rope bridge"  # NOM DANS FICHIER .XSLX
+    
+    def calculs(self, donnees, AAAA, MM, DD):
+        duration = donnees[-1, 0] * 60
+        distance = donnees[-1, 2]
+
+        maxSpeed = max(donnees[:, 1])
+        meanSpeed = (donnees[:, 1]).mean()
+        maxInclination = max(donnees[:, 5])
+        minInclination = min(donnees[:, 5])
+        meanInclination = (donnees[:, 5]).mean()
+
+        resultats = (("Jeu", self.nom), 
+                     ("Date_AAAA_MM_DD", f"{AAAA}_{MM}_{DD}"), 
+                     ("Duree_s", duration), 
+                     ("Distance_m", distance),
+                     ("VitesseMax_m/s", maxSpeed),
+                     ("VitesseMoy_m/s", meanSpeed),
+                     ("InclinaisonMax_deg", maxInclination),
+                     ("InclinaisonMin_deg", minInclination),
+                     ("InclinaisonMoy_deg", meanInclination)
+                    )
+
+        return resultats
--- JeuxAnalyses/main.py
+++ JeuxAnalyses/main.py
@@ -44,68 +44,69 @@ def Excel(ID, file, directory):
 
     wb = load_workbook(file)
     
-    # Creation des feuilles dejeu
+    # Creation des feuilles de jeu
     games = [obj for name, obj in inspect.getmembers(jeux) if inspect.isclass(obj)]
     for game in games:
-        cls = game()
-        info = cls.init()
+        classJeux = game()
         
-        if info[1] in wb.sheetnames:
-            wb.remove(wb[info[1]])
-        ws = wb.create_sheet(info[1])
-        
-        motif_recherche = f'{directory}/{ID}/Record Data/Fichiers txt/{info[0]}'
+        motif_recherche = f'{directory}/{ID}/Record Data/Fichiers txt/{classJeux.nom_txt}'
         fichiers_correspondants = glob.glob(motif_recherche, recursive=True)
         
-        i = 2
-        for fichier in fichiers_correspondants:
-            data = np.genfromtxt(fichier, delimiter="\t", skip_header=1)
-
-            nomFichier = os.path.basename(fichier)
-            partiesNom = nomFichier.split('_')
-            AAAA = partiesNom[2][0:4]
-            MM = partiesNom[2][4:6]
-            DD = partiesNom[2][6:8]
+        if classJeux.nom in wb.sheetnames:
+            wb.remove(wb[classJeux.nom])
             
-            resultats = cls.calculs(donnees=data, AAAA=AAAA, MM=MM, DD=DD)
+        if len(fichiers_correspondants) > 0:
+            ws = wb.create_sheet(classJeux.nom)
             
-            for l in range(len(resultats)):
-                ws[chr(65+l)+str(i)] = resultats[l][1]
-                ws[chr(65+l)+str(i)].number_format = "0.00"
-            i += 1
-            
-        ws[chr(65)+str(i+1)] = "Signature :"
-        bold(ws, chr(65)+str(i+1))
-
-        for l in range(len(resultats)):  # Entetes
-            ws[chr(65+l)+str(1)] = resultats[l][0]
-            bold(ws, chr(65+l)+str(1))
-            
-        # Mise en page
-        cells_to_border_1, cells_to_border_2, cells_to_border_3, cells_to_border_4, cells_to_border_5 = [], [], [], [], []
-
-        for col in range(len(resultats)):
-            cells_to_border_1.append(chr(65+col) + str(1))
-            cells_to_border_4.append(chr(65+col) + str(1))
-            cells_to_border_4.append(chr(65+col) + str(i))
-
-        for row in range(1, i):
-            cells_to_border_2.append("A" + str(row))
-            cells_to_border_5.append(chr(64+len(resultats)) + str(row))
-            for col in range(len(resultats)):
-                if row > 1 and row < i:
-                    cells_to_border_3.append(chr(65+col) + str(row))
-
-        set_border(ws, cells_to_border_1, "top", "thick")
-        set_border(ws, cells_to_border_2, "left", "thick")
-        set_border(ws, cells_to_border_3, "bottom", "thin")
-        set_border(ws, cells_to_border_4, "bottom", "thick")
-        set_border(ws, cells_to_border_5, "right", "thick")
-
-        set_alignment(ws, "center")
-        set_alignment(ws, "left", ["A"+str(i+1)])
+            i = 2
+            for fichier in fichiers_correspondants:
+                data = np.genfromtxt(fichier, delimiter="\t", skip_header=1)
     
-        optimize_cell_width(ws)
+                nomFichier = os.path.basename(fichier)
+                partiesNom = nomFichier.split('_')
+                AAAA = partiesNom[2][0:4]
+                MM = partiesNom[2][4:6]
+                DD = partiesNom[2][6:8]
+                
+                resultats = classJeux.calculs(donnees=data, AAAA=AAAA, MM=MM, DD=DD)
+                
+                for l in range(len(resultats)):
+                    ws[chr(65+l)+str(i)] = resultats[l][1]
+                    ws[chr(65+l)+str(i)].number_format = "0.00"
+                i += 1
+                
+            ws[chr(65)+str(i+1)] = "Signature :"
+            bold(ws, chr(65)+str(i+1))
+    
+            for l in range(len(resultats)):  # Entetes
+                ws[chr(65+l)+str(1)] = resultats[l][0]
+                bold(ws, chr(65+l)+str(1))
+                
+            # Mise en page
+            cells_to_border_1, cells_to_border_2, cells_to_border_3, cells_to_border_4, cells_to_border_5 = [], [], [], [], []
+    
+            for col in range(len(resultats)):
+                cells_to_border_1.append(chr(65+col) + str(1))
+                cells_to_border_4.append(chr(65+col) + str(1))
+                cells_to_border_4.append(chr(65+col) + str(i))
+    
+            for row in range(1, i):
+                cells_to_border_2.append("A" + str(row))
+                cells_to_border_5.append(chr(64+len(resultats)) + str(row))
+                for col in range(len(resultats)):
+                    if row > 1 and row < i:
+                        cells_to_border_3.append(chr(65+col) + str(row))
+    
+            set_border(ws, cells_to_border_1, "top", "thick")
+            set_border(ws, cells_to_border_2, "left", "thick")
+            set_border(ws, cells_to_border_3, "bottom", "thin")
+            set_border(ws, cells_to_border_4, "bottom", "thick")
+            set_border(ws, cells_to_border_5, "right", "thick")
+    
+            set_alignment(ws, "center")
+            set_alignment(ws, "left", ["A"+str(i+1)])
+        
+            optimize_cell_width(ws)
     wb.save(file)
 
 
