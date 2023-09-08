c62e9aeb10b03691b3c649a52225145c9d9b82f2
Pariterre <benjamin.michaud.phq@gmail.com>
2023-09-08 08:54:53 -0400

    Refactored generate_pdf

--- grail_analyses/quantified_walking_analysis/generate_pdf.py
+++ grail_analyses/quantified_walking_analysis/generate_pdf.py
@@ -10,7 +10,7 @@ from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Spacer, Pag
 from .config import movement_names
 
 
-def generate_pdf(excel_data, save_path):
+def generate_pdf(data, save_path):
 
     doc = SimpleDocTemplate(
         save_path, pagesize=letter, leftMargin=0.5 * inch, rightMargin=0.5 * inch,
@@ -29,162 +29,66 @@ def generate_pdf(excel_data, save_path):
     elements.append(identification_table)
     elements.append(Spacer(1, 20))  # Add an empty spacer for separation
 
-    # Cinématique
-    title = [["Cinématique"]]
-    title_table = Table(title, colWidths=[506])
-    title_table.setStyle(
-        TableStyle([
-            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
-            ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-        ])
-    )
-    elements.append(title_table)
-
-    phases = [["", "Phase d'Appui", "Phase d'Oscillations"]]
-    phases_table = Table(phases, colWidths=[140, 183, 183])
-    phases_table.setStyle(TableStyle([
-        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
-        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-        ('LINEBEFORE', (0, 0), (0, -1), 2, colors.black),
-        ('LINEAFTER', (-1, 0), (-1, -1), 2, colors.black),
-        ('LINEAFTER', (0, 0), (-1, -1), 1, colors.black),
-        ('LINEABOVE', (0, 0), (-1, 0), 2, colors.black),
-    ]))
-    elements.append(phases_table)
-
-    measures = [["", "Min (deg)", "Max (deg)", "Range (deg)", "Min (deg)", "Max (deg)", "Range (deg)"]]
-
-    for name, plane in zip(("Sagittal", "Frontal", "Transversal"), ("sagittal", "frontal", "transversal")):
-        _populate_measures(measures, excel_data=excel_data, name=name, level="kinematics", plane=plane)
-
-    n_sagittal = len(movement_names["kinematics"]["sagittal"]) - 1
-    n_frontal = len(movement_names["kinematics"]["frontal"]) - 1
-    measure_table = Table(measures, colWidths=[140, 61, 61, 61, 61, 61, 61])
-    measure_table.setStyle(TableStyle([
-        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
-        ('FONTNAME', (0, 1), (0, 1), 'Helvetica-Bold'),
-        ('FONTNAME', (0, n_sagittal + 3), (0, n_sagittal + 3), 'Helvetica-Bold'),
-        ('FONTNAME', (0, n_sagittal + n_frontal + 5), (0, n_sagittal + n_frontal + 5), 'Helvetica-Bold'),
-        ('LINEBEFORE', (0, 0), (0, -1), 2, colors.black),
-        ('LINEAFTER', (-1, 0), (-1, -1), 2, colors.black),
-        ('LINEAFTER', (-4, 0), (-4, -1), 1, colors.black),
-        ('LINEAFTER', (0, 0), (0, -1), 1, colors.black),
-        ('LINEBELOW', (0, n_sagittal + 2), (-1, n_sagittal + 2), 1, colors.black),
-        ('LINEBELOW', (0, n_sagittal + n_frontal + 4), (-1, n_sagittal + n_frontal + 4), 1, colors.black),
-        ('LINEBELOW', (0, 0), (-1, 0), 1, colors.black),
-        ('LINEBELOW', (0, -1), (-1, -1), 2, colors.black),
-    ]))
-
-    elements.append(measure_table)
-    elements.append(PageBreak())
-
-    # Moment
-    title = [["Moment"]]
-    title_table = Table(title, colWidths=[475])
-    title_table.setStyle(TableStyle([
-        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
-        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-    ]))
-    elements.append(title_table)
-
-    phases = [["", "Phase d'Appui", "Phase d'Oscillations"]]
-    phases_table = Table(phases, colWidths=[140, 183, 183])
-    phases_table.setStyle(TableStyle([
-        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
-        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-        ('LINEBEFORE', (0, 0), (0, -1), 2, colors.black),
-        ('LINEAFTER', (-1, 0), (-1, -1), 2, colors.black),
-        ('LINEAFTER', (0, 0), (-1, -1), 1, colors.black),
-        ('LINEABOVE', (0, 0), (-1, 0), 2, colors.black),
-    ]))
-    elements.append(phases_table)
-
-    measures = [["", "Min (deg)", "Max (deg)", "Range (deg)", "Min (deg)", "Max (deg)", "Range (deg)"]]
-    for name, plane in zip(("Sagittal", "Frontal", "Transversal"), ("sagittal", "frontal", "transversal")):
-        _populate_measures(measures, excel_data=excel_data, name=name, level="moment", plane=plane)
-
-    measure_table = Table(measures, colWidths=[140, 61, 61, 61, 61, 61, 61])
-    n_sagittal = len(movement_names["moment"]["sagittal"]) - 1
-    n_frontal = len(movement_names["moment"]["frontal"]) - 1
-    measure_table.setStyle(TableStyle([
-        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
-        ('FONTNAME', (0, 1), (0, 1), 'Helvetica-Bold'),
-        ('FONTNAME', (0, n_sagittal + 3), (0, n_sagittal + 3), 'Helvetica-Bold'),
-        ('FONTNAME', (0, n_sagittal + n_frontal + 5), (0, n_sagittal + n_frontal + 5), 'Helvetica-Bold'),
-        ('LINEBEFORE', (0, 0), (0, -1), 2, colors.black),
-        ('LINEAFTER', (-1, 0), (-1, -1), 2, colors.black),
-        ('LINEAFTER', (-4, 0), (-4, -1), 1, colors.black),
-        ('LINEAFTER', (0, 0), (0, -1), 1, colors.black),
-        ('LINEBELOW', (0, n_sagittal + 2), (-1, n_sagittal + 2), 1, colors.black),
-        ('LINEBELOW', (0, n_sagittal + n_frontal + 4), (-1, n_sagittal + n_frontal + 4), 1, colors.black),
-        ('LINEBELOW', (0, 0), (-1, 0), 1, colors.black),
-        ('LINEBELOW', (0, -1), (-1, -1), 2, colors.black),
-    ]))
-
-    elements.append(measure_table)
-    elements.append(PageBreak())
-
-    # Power
-    title = [["Puissance"]]
-    title_table = Table(title, colWidths=[475])
-    title_table.setStyle(TableStyle([
-        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
-        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-    ]))
-    elements.append(title_table)
-
-    phases = [["", "Phase d'Appui", "Phase d'Oscillations"]]
-    phases_table = Table(phases, colWidths=[140, 183, 183])
-    phases_table.setStyle(TableStyle([
-        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
-        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-        ('LINEBEFORE', (0, 0), (0, -1), 2, colors.black),
-        ('LINEAFTER', (-1, 0), (-1, -1), 2, colors.black),
-        ('LINEAFTER', (0, 0), (-1, -1), 1, colors.black),
-        ('LINEABOVE', (0, 0), (-1, 0), 2, colors.black),
-    ]))
-    elements.append(phases_table)
-
-    measures = [["", "Min (deg)", "Max (deg)", "Range (deg)", "Min (deg)", "Max (deg)", "Range (deg)"]]
-
-    for name, plane in zip(("Sagittal", "Frontal", "Transversal"), ("sagittal", "frontal", "transversal")):
-        _populate_measures(measures, excel_data=excel_data, name=name, level="power", plane=plane)
-
-    measure_table = Table(measures, colWidths=[140, 61, 61, 61, 61, 61, 61])
-    n_sagittal = len(movement_names["power"]["sagittal"]) - 1
-    n_frontal = len(movement_names["power"]["frontal"]) - 1
-    measure_table.setStyle(TableStyle([
-        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
-        ('FONTNAME', (0, 1), (0, 1), 'Helvetica-Bold'),
-        ('FONTNAME', (0, n_sagittal + 3), (0, n_sagittal + 3), 'Helvetica-Bold'),
-        ('FONTNAME', (0, n_sagittal + n_frontal + 5), (0, n_sagittal + n_frontal + 5), 'Helvetica-Bold'),
-        ('LINEBEFORE', (0, 0), (0, -1), 2, colors.black),
-        ('LINEAFTER', (-1, 0), (-1, -1), 2, colors.black),
-        ('LINEAFTER', (-4, 0), (-4, -1), 1, colors.black),
-        ('LINEAFTER', (0, 0), (0, -1), 1, colors.black),
-        ('LINEBELOW', (0, n_sagittal + 2), (-1, n_sagittal + 2), 1, colors.black),
-        ('LINEBELOW', (0, n_sagittal + n_frontal + 4), (-1, n_sagittal + n_frontal + 4), 1, colors.black),
-        ('LINEBELOW', (0, 0), (-1, 0), 1, colors.black),
-        ('LINEBELOW', (0, -1), (-1, -1), 2, colors.black),
-    ]))
-
-    elements.append(measure_table)
-    elements.append(PageBreak())
+    _create_measure_table(elements, data=data, title="Cinématique", level="kinematics")
+    _create_measure_table(elements, data=data, title="Moment", level="moment")
+    _create_measure_table(elements, data=data, title="Puissance", level="power")
 
     doc.build(elements)
     _create_page_numbered_pdf(save_path)
 
 
-def _populate_measures(measures: list, excel_data: dict, name: str, level: str, plane: str):
+def _create_measure_table(elements, data, title: str, level: str):
+    if level not in ("kinematics", "moment", "power"):
+        raise ValueError("Wrong level")
+
+    title_table = Table([[title]], colWidths=[475])
+    title_table.setStyle(TableStyle([
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+    ]))
+    elements.append(title_table)
+
+    phases = [["", "Phase d'Appui", "Phase d'Oscillations"]]
+    phases_table = Table(phases, colWidths=[140, 183, 183])
+    phases_table.setStyle(TableStyle([
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('LINEBEFORE', (0, 0), (0, -1), 2, colors.black),
+        ('LINEAFTER', (-1, 0), (-1, -1), 2, colors.black),
+        ('LINEAFTER', (0, 0), (-1, -1), 1, colors.black),
+        ('LINEABOVE', (0, 0), (-1, 0), 2, colors.black),
+    ]))
+    elements.append(phases_table)
+
+    measures = [["", "Min (deg)", "Max (deg)", "Range (deg)", "Min (deg)", "Max (deg)", "Range (deg)"]]
+    for name, plane in zip(("Sagittal", "Frontal", "Transversal"), ("sagittal", "frontal", "transversal")):
+        _populate_measures(measures, data=data, name=name, level=level, plane=plane)
+    measure_table = Table(measures, colWidths=[140, 61, 61, 61, 61, 61, 61])
+    n_sagittal = len(movement_names[level]["sagittal"]) - 1
+    n_frontal = len(movement_names[level]["frontal"]) - 1
+    measure_table.setStyle(TableStyle([
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+        ('FONTNAME', (0, 1), (0, 1), 'Helvetica-Bold'),
+        ('FONTNAME', (0, n_sagittal + 3), (0, n_sagittal + 3), 'Helvetica-Bold'),
+        ('FONTNAME', (0, n_sagittal + n_frontal + 5), (0, n_sagittal + n_frontal + 5), 'Helvetica-Bold'),
+        ('LINEBEFORE', (0, 0), (0, -1), 2, colors.black),
+        ('LINEAFTER', (-1, 0), (-1, -1), 2, colors.black),
+        ('LINEAFTER', (-4, 0), (-4, -1), 1, colors.black),
+        ('LINEAFTER', (0, 0), (0, -1), 1, colors.black),
+        ('LINEBELOW', (0, n_sagittal + 2), (-1, n_sagittal + 2), 1, colors.black),
+        ('LINEBELOW', (0, n_sagittal + n_frontal + 4), (-1, n_sagittal + n_frontal + 4), 1, colors.black),
+        ('LINEBELOW', (0, 0), (-1, 0), 1, colors.black),
+        ('LINEBELOW', (0, -1), (-1, -1), 2, colors.black),
+    ]))
+    elements.append(measure_table)
+
+    elements.append(PageBreak())
+
+
+def _populate_measures(measures: list, data: dict, name: str, level: str, plane: str):
     if level not in ("kinematics", "moment", "power"):
         raise ValueError("Wrong level")
     if plane not in ("sagittal", "frontal", "transversal"):
@@ -195,12 +99,12 @@ def _populate_measures(measures: list, excel_data: dict, name: str, level: str, 
     for i in range(len(names)):
         row = [
             names[i]["fr"],
-            round(excel_data[names[i]["en"]]["Minimum Appui"], 2),
-            round(excel_data[names[i]["en"]]["Maximum Appui"], 2),
-            round(excel_data[names[i]["en"]]["Range Appui"], 2),
-            round(excel_data[names[i]["en"]]["Minimum Oscillation"], 2),
-            round(excel_data[names[i]["en"]]["Maximum Oscillation"], 2),
-            round(excel_data[names[i]["en"]]["Range Oscillation"], 2),
+            round(data[names[i]["en"]]["Minimum Appui"], 2),
+            round(data[names[i]["en"]]["Maximum Appui"], 2),
+            round(data[names[i]["en"]]["Range Appui"], 2),
+            round(data[names[i]["en"]]["Minimum Oscillation"], 2),
+            round(data[names[i]["en"]]["Maximum Oscillation"], 2),
+            round(data[names[i]["en"]]["Range Oscillation"], 2),
         ]
         measures.append(row)
 
